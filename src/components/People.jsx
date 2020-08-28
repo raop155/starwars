5231be67991ba2a4647297d6483dba98630960b9
Ricardo Olarte <raop155@hotmail.com>
2020-08-28 11:59:00 -0500

    feature(people) Add dynamic pagination to People list

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useQuery } from 'react-query';
+import { usePaginatedQuery } from 'react-query';
 import Person from './Person';
 
 const fetchPeople = async (key, page) => {
@@ -9,7 +9,7 @@ const fetchPeople = async (key, page) => {
 
 const People = () => {
   const [page, setPage] = useState(1);
-  const { data, status } = useQuery(['people', page], fetchPeople, {
+  const { resolvedData, latestData, status } = usePaginatedQuery(['people', page], fetchPeople, {
     // staleTime: 5000,
     // cacheTime: 5000,
     onSuccess: () => console.log('data fetch with no problemo!'),
@@ -19,29 +19,35 @@ const People = () => {
   return (
     <>
       <h2 className='subtitle is-3 mt-3'>People</h2>
-      <div className='buttons'>
-        <button
-          className='button is-dark is-outlined is-inverted is-focused'
-          onClick={() => setPage(1)}
-        >
-          Page 1
-        </button>
-        <button className='button is-dark is-outlined is-inverted' onClick={() => setPage(2)}>
-          Page 2
-        </button>
-        <button className='button is-dark is-outlined is-inverted' onClick={() => setPage(3)}>
-          Page 3
-        </button>
-      </div>
 
       {status === 'loading' && <div>Loading data...</div>}
       {status === 'error' && <div>Error fetching data</div>}
       {status === 'success' && (
-        <ul>
-          {data.results.map((person) => {
-            return <Person key={person.name} person={person} />;
-          })}
-        </ul>
+        <>
+          <ul className='mb-5'>
+            {resolvedData.results.map((person) => {
+              return <Person key={person.name} person={person} />;
+            })}
+          </ul>
+
+          <div className='buttons'>
+            <button
+              className='button is-dark is-outlined is-inverted'
+              onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+              disabled={page === 1}
+            >
+              Previous Page
+            </button>
+            <span className='mr-2 mb-2 is-size-5'>{page}</span>
+            <button
+              className='button is-dark is-outlined is-inverted'
+              onClick={() => setPage((prev) => (!latestData || !latestData.next ? prev : prev + 1))}
+              disabled={!latestData || !latestData.next}
+            >
+              Next Page
+            </button>
+          </div>
+        </>
       )}
     </>
   );
--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -31,24 +31,6 @@ const Planets = () => {
       {status === 'error' && <div>Error fetching data</div>}
       {status === 'success' && (
         <>
-          {/* <div id='buttons' className='buttons'>
-            <button
-              className='button is-dark is-outlined is-inverted'
-              onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
-              disabled={page === 1}
-            >
-              Previous Page
-            </button>
-            <span className='mr-2 mb-2 is-size-5'>{page}</span>
-            <button
-              className='button is-dark is-outlined is-inverted'
-              onClick={() => setPage((prev) => (!latestData || !latestData.next ? prev : prev + 1))}
-              disabled={!latestData || !latestData.next}
-            >
-              Next Page
-            </button>
-          </div> */}
-
           <ul className='mb-5'>
             {resolvedData.results.map((planet) => {
               return <Planet key={planet.name} planet={planet} />;
@@ -63,7 +45,6 @@ const Planets = () => {
             >
               Previous Page
             </button>
-            {/* <button className='button'>{page}</button> */}
             <span className='mr-2 mb-2 is-size-5'>{page}</span>
             <button
               className='button is-dark is-outlined is-inverted'
